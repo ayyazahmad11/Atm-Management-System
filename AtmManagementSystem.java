5f9decd5fb7d54bb9b66e876a594ad4f0df40f53
Ahmad <102008302+ayyazahmad11@users.noreply.github.com>
2023-04-17 08:02:07 +0530

    Update AtmManagementSystem.java

--- AtmManagementSystem.java
+++ AtmManagementSystem.java
@@ -9,21 +9,21 @@ public class AtmManagementSystem {
 
         while (true) {
             System.out.println("*****Automated Teller Machine*****\n           Main Menu");
-            System.out.println("Choose 1 for Withdraw \n");
-            System.out.println("Choose 2 for Deposit\n");
-            System.out.println("Choose 3 for Check Balance\n");
-            System.out.println("Choose 4 for EXIT\n");
-            System.out.print("Choose the operation you want to perform:\n");
+            System.out.println("Choose 1 for Withdraw");
+            System.out.println("Choose 2 for Deposit");
+            System.out.println("Choose 3 for Check Balance");
+            System.out.println("Choose 4 for EXIT");
+            System.out.print("Choose the operation you want to perform:");
             int choice = sc.nextInt();
             switch (choice) {
                 case 1:
-                    System.out.print("Enter money to be withdrawn:\n");
+                    System.out.print("Enter money to be withdrawn:");
                     int withdraw = sc.nextInt();
                     performWithdrawal(withdraw);
                     break;
 
                 case 2:
-                    System.out.print("Enter money to be deposited:\n");
+                    System.out.print("Enter money to be deposited:");
                     int deposit = sc.nextInt();
                     performDeposit(deposit);
                     break;
@@ -33,8 +33,12 @@ public class AtmManagementSystem {
                     break;
 
                 case 4:
+                    System.out.println("Thank you for using the ATM. Exiting...");
                     System.exit(0);
+                    break;
 
+                default:
+                    System.out.println("Invalid choice. Please choose a valid option.");
             }
         }
     }
@@ -42,16 +46,16 @@ public class AtmManagementSystem {
     static void performWithdrawal(int amount) {
         if (balance >= amount) {
             balance = balance - amount;
-            System.out.println("Please collect your money\n Transiction Recorded");
+            System.out.println("Please collect your money");
         } else {
-            System.out.println("Insufficient Balance\nTransiction Recorded");
+            System.out.println("Insufficient Balance");
         }
         System.out.println("");
     }
 
     static void performDeposit(int amount) {
         balance = balance + amount;
-        System.out.println("Your Money has been successfully deposited\nTransiction Recorded");
+        System.out.println("Your Money has been successfully deposited");
         System.out.println("");
     }
 
